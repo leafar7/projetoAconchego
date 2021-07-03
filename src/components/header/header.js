b377c24d6aed333c7992cba20ae82ec7a61f52bc
leafar7 <rafelvieira77@hotmail.com>
2021-07-03 12:27:50 -0300

    changes background image test

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -29,16 +29,20 @@ export default class Header extends Component {
               
               <hr />
               <ul className="social">
-                {/* <li><a href="#"><i className="fa fa-facebook" /></a></li> 
-              <li><a href="#"><i className="fa fa-twitter" /></a></li>
-              <li><a href="#"><i className="fa fa-google-plus" /></a></li> }
-                <li><a href="https://linkedin.com/in/rafael-cano-a1239b3b/"><i className="fa fa-linkedin" /></a></li>
-                <li><a href="https://github.com/leafar7"><i className="fa fa-github" /></a></li>*/
-                <li><a href="https://www.instagram.com/grupo_aconchego/"><i className="fa fa-instagram" /></a></li>
-             /* <li><a href="#"><i className="fa fa-dribbble" /></a></li>
-            <li><a href="#"><i className="fa fa-skype" /></a></li> */}
+                {
+                  <li><a href="https://www.facebook.com/groups/2158607287616757"><i className="fa fa-facebook" /></a></li>}
+                  {/* 
+                  <li><a href="#"><i className="fa fa-twitter" /></a></li>
+                  <li><a href="#"><i className="fa fa-google-plus" /></a></li> 
+                  <li><a href="https://linkedin.com/in/rafael-cano-a1239b3b/"><i className="fa fa-linkedin" /></a></li>
+                  <li><a href="https://github.com/leafar7"><i className="fa fa-github" /></a></li>
+                  */
+                  <li><a href="https://www.instagram.com/grupo_aconchego/"><i className="fa fa-instagram" /></a></li>
+                  /*<li><a href="#"><i className="fa fa-dribbble" /></a></li>
+                  <li><a href="#"><i className="fa fa-skype" /></a></li> 
+                */}            
               </ul>
-            </div>
+             </div>
           </div>
           <p className="scrolldown">
             <a className="smoothscroll" href="#about"><i className="icon-down-circle" /></a>
