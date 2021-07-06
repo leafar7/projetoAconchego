26a92f8fc5b7e8d4e8ddf7b92feb0ebdc88de8d2
leafar7 <rafelvieira77@hotmail.com>
2021-07-05 21:49:38 -0300

    change about 1

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -24,14 +24,14 @@ export default class Header extends Component {
           <div className="row banner">
             <div className="banner-text">
               <h1 className="responsive-headline">{resumeData.name}</h1>
-              <h3>{resumeData.role} {resumeData.roleDescription}.</h3>
-              
-              
+              <h2>{resumeData.role} {resumeData.roleDescription}.</h2>
+
+
               <hr />
               <ul className="social">
                 {
                   <li><a href="https://www.facebook.com/groups/2158607287616757"><i className="fa fa-facebook" /></a></li>}
-                  {/* 
+                {/* 
                   <li><a href="#"><i className="fa fa-twitter" /></a></li>
                   <li><a href="#"><i className="fa fa-google-plus" /></a></li> 
                   <li><a href="https://linkedin.com/in/rafael-cano-a1239b3b/"><i className="fa fa-linkedin" /></a></li>
@@ -40,17 +40,17 @@ export default class Header extends Component {
                   <li><a href="https://www.instagram.com/grupo_aconchego/"><i className="fa fa-instagram" /></a></li>
                   /*<li><a href="#"><i className="fa fa-dribbble" /></a></li>
                   <li><a href="#"><i className="fa fa-skype" /></a></li> 
-                */}            
+                */}
               </ul>
-             </div>
+            </div>
           </div>
           <p className="scrolldown">
             <a className="smoothscroll" href="#about"><i className="icon-down-circle" /></a>
           </p>
-          
+
         </header> {/* Header End */}
-       
-            </React.Fragment>
+
+      </React.Fragment>
     )
   }
 }
